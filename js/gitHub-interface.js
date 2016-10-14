38a3edee381e73b0f4af40ae63dfa84970231ae7
nicolas <nickh93@outlook.com>
2016-10-14 16:09:33 -0400

    refactor code to display repo information and description in a list

--- js/gitHub-interface.js
+++ js/gitHub-interface.js
@@ -1,15 +1,11 @@
 var User = require('./../js/gitHub.js').userModule;
 
-var displayUserData = function(username, login)
-{
-  $(".showUser").text(username + "'s login is " + login  + ".");
-}
 
 $(document).ready(function() {
   var currentUserObject = new User();
   $("#usernameInput").click(function() {
       var username = $("#username").val();
       $("#username").val("");
-      currentUserObject.getUser(username, displayUserData);
+      currentUserObject.getUser(username);
   });
 });
--- js/gitHub.js
+++ js/gitHub.js
@@ -4,13 +4,18 @@ User = function() {
 
 }
 
-User.prototype.getUser = function(username, displayFunction)
+User.prototype.getUser = function(username)
 {
-    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response) {
-      displayFunction(username, response.login);
-    }).fail(function(error){
-      $('.showUser').text(error.responseJSON.message);
-    });
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response) {
+    for (var index = 0; index < response.length; index ++)
+    {
+      $(".showUser").append("<li> Name: " + response[index].name + "<li> Description: " + response[index].description + "</li>" + "</li>");
+    }
+    // displayFunction(username, response.login);
+    // console.log(response);
+  }).fail(function(error){
+    $('.showUser').text(error.responseJSON.message);
+  });
 
 }
 
