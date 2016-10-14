204bd41c20db887aacec9a2799d21ba6ddde8ed5
nicolas <nickh93@outlook.com>
2016-10-14 14:27:17 -0400

    fix a big that was not allowing the jqery function get the username from the form

--- js/gitHub-interface.js
+++ js/gitHub-interface.js
@@ -1,15 +1,15 @@
-var User = require('./../js/gitHub.js');
+var User = require('./../js/gitHub.js').userModule;
 
 var displayUserData = function(username, login)
 {
   $(".showUser").text(username + "'s login is " + login  + ".");
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
   var currentUserObject = new User();
   $("#usernameInput").click(function() {
-    var username = $("#location").val();
-    $("#location").val("");
-    currentUserObject.getUser(username, displayUserData);
+      var username = $("#username").val();
+      $("#username").val("");
+      currentUserObject.getUser(username, displayUserData);
   });
 });
--- js/gitHub.js
+++ js/gitHub.js
@@ -6,7 +6,7 @@ User = function() {
 
 User.prototype.getUser = function(username, displayFunction)
 {
-    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response) {
       displayFunction(username, response.login);
     }).fail(function(error){
       $('.showUser').text(error.responseJSON.message);
