b579f37a9def8a28a4ba6d85cc934354467dd65b
nicolas <nickh93@outlook.com>
2016-10-14 13:41:16 -0400

    create the back-end User.js and front end User-interface.js files

---
+++ js/gitHub-interface.js
@@ -0,0 +1,15 @@
+var User = require('./../js/gitHib.js');
+
+var displayUserData = function(username, login)
+{
+  $(".showUser").text(username + "'s login is " + login  + ".");
+}
+
+$(document).ready(function () {
+  var currentUserObject = new User();
+  $("#usernameInput").click(function() {
+    var username = $("#location").val();
+    $("#location").val("");
+    currentUserObject.getUser(username, displayUserData);
+  })
+});
---
+++ js/gitHub.js
@@ -0,0 +1,17 @@
+var apiKey = require('./../.env').apiKey;
+
+User = function() {
+
+}
+
+User.prototype.getUser = function(username, displayFunction)
+{
+    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+      displayFunction(username, response.login);
+    }).fail(function(error){
+      $('.showUser').text(error.responseJSON.message);
+    });
+
+}
+
+exports.userModule = User;
